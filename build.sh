88b3091f9df854a14a7fa3b8b0d9c41c8156c52e
MedzikUser <medzik@duck.com>
2022-07-25 13:53:45 +0200

    ci: refactor

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 mkdir -p target
 
-cd target && git clone https://github.com/rafi612/serverd && cd serverd && mvn install && cd ../..
+cd target
+git clone https://github.com/rafi612/serverd
+cd serverd
+mvn install
+cd ../..
 
 mvn clean package
